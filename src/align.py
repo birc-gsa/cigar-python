0ad4c1bc6d75c74e9a804b61e2dd0ea105efefc1
Thomas Mailund <mailund@birc.au.dk>
2022-03-13 15:05:59 +0100

    solution

--- src/align.py
+++ src/align.py
@@ -11,29 +11,20 @@ def get_edits(p: str, q: str) -> tuple[str, str, str]:
     Returns:
         str: The list of edit operations as a string.
 
-    >>> edits('ACCACAGT-CATA', 'A-CAGAGTACAAA')
+    >>> get_edits('ACCACAGT-CATA', 'A-CAGAGTACAAA')
     ('ACCACAGTCATA', 'ACAGAGTACAAA', 'MDMMMMMMIMMMM')
 
     """
-    return ""
-
-
-def align(p: str, q: str, edits: str) -> tuple[str, str]:
-    """Align two sequences from a sequence of edits.
-
-    Args:
-        p (str): The first sequence to align.
-        q (str): The second sequence to align
-        edits (str): The list of edits to apply, given as a string
-
-    Returns:
-        tuple[str, str]: The two rows in the pairwise alignment
-
-    >>> align("ACCACAGTCATA", "ACAGAGTACAAA", "MDMMMMMMIMMMM")
-    ('ACCACAGT-CATA', 'A-CAGAGTACAAA')
-
-    """
-    return "", ""
+    assert len(p) == len(q)
+    edits = [None] * len(p)
+    for i, _ in enumerate(p):
+        if p[i] == '-':
+            edits[i] = 'I'
+        elif q[i] == '-':
+            edits[i] = 'D'
+        else:
+            edits[i] = 'M'
+    return (p.replace('-', ''), q.replace('-', ''), "".join(edits))
 
 
 def local_align(p: str, x: str, i: int, edits: str) -> tuple[str, str]:
@@ -52,4 +43,42 @@ def local_align(p: str, x: str, i: int, edits: str) -> tuple[str, str]:
     ('ACCACAGT-CATA', 'A-CAGAGTACAAA')
 
     """
-    return "", ""
+    j, k = 0, 0
+    row_p, row_q = [], []
+    for op in edits:
+        match op:
+            case 'M':
+                row_p.append(p[j])
+                row_q.append(x[i+k])
+                j += 1
+                k += 1
+            case 'D':
+                row_p.append(p[j])
+                row_q.append('-')
+                j += 1
+            case 'I':
+                row_p.append('-')
+                row_q.append(x[i+k])
+                k += 1
+
+    return "".join(row_p), "".join(row_q)
+
+
+def align(p: str, q: str, edits: str) -> tuple[str, str]:
+    """Align two sequences from a sequence of edits.
+
+    Args:
+        p (str): The first sequence to align.
+        q (str): The second sequence to align
+        edits (str): The list of edits to apply, given as a string
+
+    Returns:
+        tuple[str, str]: The two rows in the pairwise alignment
+
+    >>> align("ACCACAGTCATA", "ACAGAGTACAAA", "MDMMMMMMIMMMM")
+    ('ACCACAGT-CATA', 'A-CAGAGTACAAA')
+
+    """
+    # A full alignment here is just one that starts at index zero.
+    # If the strings are valid input, all of q will be aligned.
+    return local_align(p, q, 0, edits)
