744519d7ebdbc18330b358d0b1fc0f5f487b2b9f
Thomas Mailund <mailund@birc.au.dk>
2022-03-13 09:50:13 +0100

    added local_align

---
+++ src/align.py
@@ -0,0 +1,55 @@
+"""A module for translating between alignments and edits sequences."""
+
+
+def get_edits(p: str, q: str) -> tuple[str, str, str]:
+    """Extract the edit operations from a pairwise alignment.
+
+    Args:
+        p (str): The first row in the pairwise alignment.
+        1 (str): The second row in the pairwise alignment.
+
+    Returns:
+        str: The list of edit operations as a string.
+
+    >>> edits('ACCACAGT-CATA', 'A-CAGAGTACAAA')
+    ('ACCACAGTCATA', 'ACAGAGTACAAA', 'MDMMMMMMIMMMM')
+
+    """
+    return ""
+
+
+def align(p: str, q: str, edits: str) -> tuple[str, str]:
+    """Align two sequences from a sequence of edits.
+
+    Args:
+        p (str): The first sequence to align.
+        q (str): The second sequence to align
+        edits (str): The list of edits to apply, given as a string
+
+    Returns:
+        tuple[str, str]: The two rows in the pairwise alignment
+
+    >>> align("ACCACAGTCATA", "ACAGAGTACAAA", "MDMMMMMMIMMMM")
+    ('ACCACAGT-CATA', 'A-CAGAGTACAAA')
+
+    """
+    return "", ""
+
+
+def local_align(p: str, x: str, i: int, edits: str) -> tuple[str, str]:
+    """Align two sequences from a sequence of edits.
+
+    Args:
+        p (str): The read string we have mapped against x
+        x (str): The longer string we have mapped against
+        i (int): The location where we have an approximative match
+        edits (str): The list of edits to apply, given as a string
+
+    Returns:
+        tuple[str, str]: The two rows in the pairwise alignment
+
+    >>> local_align("ACCACAGTCATA", "GTACAGAGTACAAA", 2, "MDMMMMMMIMMMM")
+    ('ACCACAGT-CATA', 'A-CAGAGTACAAA')
+
+    """
+    return "", ""
---
+++ src/test_align.py
@@ -0,0 +1,42 @@
+"""Tests for the align module."""
+
+from align import (
+    align,
+    get_edits,
+    local_align
+)
+
+
+def test_edits() -> None:
+    """Testing the edits() function."""
+    assert get_edits(
+        "acca-aagt--a", "a-caaatgtcca"
+    ) == ("accaaagta", "acaaatgtcca", "MDMMIMMMMIIM")
+    assert get_edits(
+        "acgttcga", "aaa---aa"
+    ) == ("acgttcga", "aaaaa", "MMMDDDMM")
+
+
+def test_align() -> None:
+    """Testing the align() function."""
+    assert align(
+        "accaaagta",
+        "acaaatgtcca",
+        "MDMMIMMMMIIM"
+    ) == ("acca-aagt--a", "a-caaatgtcca")
+    assert align(
+        "a", "", "1D"
+    ) == ("a", "-")
+
+
+def test_local_align() -> None:
+    """Testing the local_align() function."""
+    assert local_align(
+        "accaaagta",
+        "gtacaaatgtcca",
+        2,
+        "MDMMIMMMMIIM"
+    ) == ("acca-aagt--a", "a-caaatgtcca")
+    assert local_align(
+        "a", "", 0, "1D"
+    ) == ("a", "-")
